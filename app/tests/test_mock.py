5fb4ca5439e8b3a48a0d79d05505cfef6b8b3df4
IgnacioManes <nacho.maness@gmail.com>
2018-09-12 23:03:35 -0300

    preuba 2

--- app/tests/test_mock.py
+++ app/tests/test_mock.py
@@ -3,10 +3,11 @@ import pytest
 from flask_pymongo import PyMongo
 from flask import Flask
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://admin:Taller2018@ds245532.mlab.com:45532/app-server'
+with app.app_context():
+    app.database = PyMongo(app).db
 def test_mock():
 	assert 1==1
 
 def test_db():
-	app.config['MONGO_URI'] = 'mongodb://admin:Taller2018@ds245532.mlab.com:45532/app-server'
-	app.database = PyMongo(app).db
 	assert app.database.prueba.findone() is not None
