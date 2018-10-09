470d4d37f88c95ea147ab4efc0fce854485b27a6
IgnacioManes <nacho.maness@gmail.com>
2018-10-09 18:37:03 -0300

    Revert "coveralls test 2"
    This reverts commit 3fb55d0bf4f00b81166d91a8077f771a2d562553.

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 cd app/
 
 python -m pytest tests/* --cov-config .coveragerc --cov=$(pwd)/server
-COVERALLS_REPO_TOKEN=9x7MsRmf6WGLZOCLpvRTW55OOxqZA63NeT0RL
+COVERALLS_REPO_TOKEN=9x7MsRmf6WGLZOCLpvRTW55OOxqZA63NeT0RL coveralls
