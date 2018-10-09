2c99d6fa0a121d4e659d2dc46435961ffeb7c507
IgnacioManes <nacho.maness@gmail.com>
2018-10-09 18:44:20 -0300

    ERA EL TOKEN

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 cd app/
 
 python -m pytest tests/* --cov-config .coveragerc --cov=$(pwd)/server
-COVERALLS_REPO_TOKEN=9x7MsRmf6WGLZOCLpvRTW55OOxqZA63NeT0RL coveralls
+COVERALLS_REPO_TOKEN=ZhlPFnicY6u0teN8LKtKAS2DbuuKxWTio coveralls
