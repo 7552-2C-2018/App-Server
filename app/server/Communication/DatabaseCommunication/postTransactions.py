80cccfe8fc4baed44e210618e006a0c999187703
nacho <nacho.maness@gmail.com>
2018-12-04 02:13:22 -0300

    integracion del puntaje de usuario con los posts, agregadas las definiciones de puntos (falta vincular la actualizacion de los mismos con las compras y ventas)

--- app/server/Communication/DatabaseCommunication/postTransactions.py
+++ app/server/Communication/DatabaseCommunication/postTransactions.py
@@ -8,6 +8,7 @@ import base64
 import qrcode
 from io import BytesIO
 import re
+from bson.son import SON
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 with app.app_context():
@@ -59,11 +60,57 @@ class PostTransactions:
     @staticmethod
     def getPosts(data):
         queryFilters = PostTransactions.__build_query_filters(data)
-        response = list(workingCollection.find(queryFilters,
-                                               {"_id": 0, "ID": 1, "title": 1, "price": 1,
-                                                "coordenates": 1, 'pictures': {'$slice': 1}}
-                                               ).sort("price", pymongo.ASCENDING))
-        return response
+        pipeline = [
+            {
+                u"$project": {
+                    u"_id": 0,
+                    u"posts": u"$$ROOT"
+                }
+            },
+            {
+                u"$lookup": {
+                    u"localField": u"posts._id.facebookId",
+                    u"from": u"users",
+                    u"foreignField": u"facebookId",
+                    u"as": u"users"
+                }
+            },
+            {
+                u"$unwind": {
+                    u"path": u"$users",
+                    u"preserveNullAndEmptyArrays": False
+                }
+            },
+            {
+                u"$match":
+                    queryFilters
+
+            },
+            {
+                u"$project": {
+                    u"price": u"$posts.price",
+                    u"ID": u"$posts.ID",
+                    u"title": u"$posts.title",
+                    u"coordenates": u"$posts.coordenates",
+                    #u'pictures': {"$slice": ["$posts.pictures", 0, 1]},
+                    u'totalPoints': u"$users.totalPoints"
+                }
+
+            },
+            {
+                u"$sort": SON([(u"users.totalPoints", -1)])
+            }
+        ]
+
+        cursor = workingCollection.aggregate(
+            pipeline,
+            allowDiskUse=True
+        )
+        # response = list(workingCollection.find(queryFilters,
+        #                                        {"_id": 0, "ID": 1, "title": 1, "price": 1,
+        #                                         "coordenates": 1, 'pictures': {'$slice': 1}}
+        #                                       ))
+        return list(cursor)
 
     @staticmethod
     def new_post(data):
@@ -97,32 +144,32 @@ class PostTransactions:
     def __build_query_filters(data):
         filters = {}
         if data['distancia'] is not None and data['latitud'] is not None and data['longitud'] is not None:
-            filters["coordenates"] = {}
-            filters["coordenates"]["$geoWithin"] = {}
-            filters["coordenates"]["$geoWithin"]["$center"] = [[data['longitud'], data['latitud']], data['distancia']]
+            filters["posts.coordenates"] = {}
+            filters["posts.coordenates"]["$geoWithin"] = {}
+            filters["posts.coordenates"]["$geoWithin"]["$center"] = [[data['longitud'], data['latitud']], data['distancia']]
 
         if data['precioMinimo'] is not None or data['precioMaximo'] is not None:
-            filters["price"] = {}
+            filters["posts.price"] = {}
         if data['precioMaximo'] is not None:
-            filters['price']['$lte'] = data['precioMaximo']
+            filters['posts.price']['$lte'] = data['precioMaximo']
         if data['precioMinimo'] is not None:
-            filters['price']['$gte'] = data['precioMinimo']
+            filters['posts.price']['$gte'] = data['precioMinimo']
         if data['estado'] is not None:
             if data['estado'] == "nuevo":
-                filters['new'] = True
+                filters['posts.new'] = True
             else:
-                filters['new'] = False
+                filters['posts.new'] = False
         if data['envio'] is not None:
             if data["envio"] == 1:
-                filters['shipping'] = True
+                filters['posts.shipping'] = True
             if data["envio"] == 0:
-                filters['shipping'] = False
+                filters['posts.shipping'] = False
         if data['categoria'] is not None:
-            filters['category'] = re.compile(data['categoria'], re.IGNORECASE)
+            filters['posts.category'] = re.compile(data['categoria'], re.IGNORECASE)
         if data['search'] is not None:
-            filters['title'] = {}
+            filters['posts.title'] = {}
 
-            filters['title']["$regex"] = re.compile(data['search'], re.IGNORECASE)
+            filters['posts.title']["$regex"] = re.compile(data['search'], re.IGNORECASE)
         return filters
 
     @staticmethod
--- app/server/Communication/DatabaseCommunication/userTransactions.py
+++ app/server/Communication/DatabaseCommunication/userTransactions.py
@@ -4,8 +4,14 @@ import logging
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 with app.app_context():
-    workingCollection = app.database.users
+    userCollection = app.database.users
 
+SCORE_MULTIPLIER = 100
+BUY_SCORE = 20
+SELL_SCORE = 20
+CARD_POINTS = 5
+MERCADO_PAGO_POINTS = 10
+CASH_POINTS = 0
 
 class UserTransactions:
 
@@ -27,16 +33,16 @@ class UserTransactions:
 
     @staticmethod
     def findUserById(facebook_id):
-        return workingCollection.find_one({'facebookId': facebook_id})
+        return userCollection.find_one({'facebookId': facebook_id})
 
     @staticmethod
     def getUserName(facebook_id):
-        return workingCollection.find_one({'facebookId': facebook_id}, {"nombre": 1, "apellido": 1, "_id": 0})
+        return userCollection.find_one({'facebookId': facebook_id}, {"nombre": 1, "apellido": 1, "_id": 0})
 
     @staticmethod
     def newUser(user_id, first_name, last_name, photo_url, email):
 
-        id = workingCollection.insert_one({"facebookId": user_id,
+        id = userCollection.insert_one({"facebookId": user_id,
                                            "nombre": first_name,
                                            "apellido": last_name,
                                            "photoUrlToken": photo_url,
@@ -45,29 +51,30 @@ class UserTransactions:
 
     @staticmethod
     def updateUserToken(facebook_id, new_token, new_expdate):
-        workingCollection.update_one({'facebookId': facebook_id},
+        userCollection.update_one({'facebookId': facebook_id},
                                      {'$set': {'token': new_token, 'exp_date': new_expdate}})
 
     @staticmethod
     def updateUserBuyPoints(facebook_id, payment_method):
-        points = 0
-        if payment_method == "":
-            points = 0
-        elif payment_method == "":
-            points = 0
-        elif payment_method == "":
-            points = 0
-        elif payment_method == "":
-            points = 0
-        workingCollection.update_one({'facebookId': facebook_id},
-                                     {'$set': {'$inc': {'buyPoints': points, 'totalPoints': points}}})
+        points = BUY_SCORE
+        if payment_method == "Debito" or payment_method == "Credito":
+            points += CARD_POINTS
+        elif payment_method == "Efectivo" \
+                or payment_method == "Transferencia" \
+                or payment_method == "Cheque"\
+                or payment_method == "DepositoBancario":
+            points += CASH_POINTS
+        elif payment_method == "MercadoPago":
+            points += MERCADO_PAGO_POINTS
+        userCollection.update_one({'facebookId': facebook_id},
+                                     {'$set': {'$inc': {'buyPoints': points}}})
         UserTransactions.__update_total_score(facebook_id)
-        
+
     @staticmethod
     def updateUserSellPoints(facebook_id):
-        points = 0
-        workingCollection.update_one({'facebookId': facebook_id},
-                                     {'$set': {'$inc': {'sellPoints': points, 'totalPoints': points}}})
+        points = SCORE_MULTIPLIER
+        userCollection.update_one({'facebookId': facebook_id},
+                                     {'$set': {'$inc': {'sellPoints': points}}})
         UserTransactions.__update_total_score(facebook_id)
 
     @staticmethod
@@ -75,7 +82,7 @@ class UserTransactions:
         points = int(score_average*100)
         logging.debug(facebook_id)
         logging.debug(points)
-        workingCollection.update_one({'facebookId': facebook_id},
+        userCollection.update_one({'facebookId': facebook_id},
                                      {'$set': {'scorePoints': points}})
         UserTransactions.__update_total_score(facebook_id)
 
@@ -84,20 +91,30 @@ class UserTransactions:
     def updateUserData(data):
         parsed_data = UserTransactions.__parseUpdateData(data)
         if parsed_data:
-            workingCollection.update_one({'facebookId': data['facebookId']}, {'$set': parsed_data})
+            userCollection.update_one({'facebookId': data['facebookId']}, {'$set': parsed_data})
 
     @staticmethod
     def getUserActivities(facebook_id):
-        return workingCollection.find_one({'facebookId': facebook_id}, {"activities": 1})
+        return userCollection.find_one({'facebookId': facebook_id}, {"activities": 1})
 
     @staticmethod
     def pushUserActivitiy(facebook_id, data):
-        return workingCollection.update_one({'facebookId': facebook_id}, {'$set': {'$push': {'activities': data}}})
+        return userCollection.update_one({'facebookId': facebook_id}, {'$set': {'$push': {'activities': data}}})
 
     @staticmethod
-    def __update_total_score( facebook_id):
-        pass
-
+    def __update_total_score(facebook_id):
+        result = userCollection.find_one({'facebookId': facebook_id},
+                                         {"sellPoints": 1, "buyPoints": 1, "scorePoints": 1})
+        if "sellPoints" not in result.keys():
+            result["sellPoints"] = 0
+        if "buyPoints" not in result.keys():
+            result["buyPoints"] = 0
+        if "scorePoints" not in result.keys():
+            result["scorePoints"] = 0
+        points = result["sellPoints"]
+        points += result["buyPoints"]
+        points += result["scorePoints"]
+        userCollection.update_one({'facebookId': facebook_id}, {'$set': {'totalPoints': points}})
 
 """    @staticmethod
     def __getCalification(facebook_id):
